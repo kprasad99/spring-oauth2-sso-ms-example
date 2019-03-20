436acf678e2fd4a8a246f190f42a19f68c62b52f
Karthik Prasad <kprasad@infinera.com>
2019-03-20 10:04:59 +0530

    Remove cookie

--- app2/src/app/auth/interceptors/token-clear.interceptor.ts
+++ app2/src/app/auth/interceptors/token-clear.interceptor.ts
@@ -3,19 +3,20 @@
 
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
-export class ToekClearInterceptor implements HttpInterceptor {
+export class TokenClearInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService) { }
+    constructor(private cookieService: CookieService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(tap(req => {
             if (request.url === '/oauth/revoke_token') {
                 localStorage.removeItem('currentUser');
+                this.cookieService.delete('JSESSIONID');
             }
         }));
     }
